3b51d4868c93c9f240cef55a04183ec50c6fbdff
Valor Naram <valinora@gmx.net>
2025-07-15 22:13:23 +0200

    feat: added test cases for a table without row level headers but with a single header column

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,21 +1,32 @@
-import { expect, describe, test } from "@jest/globals"
+import { expect, describe, test } from "@jest/globals";
 
-import complicatedTableModel from "./assets/conceptual_complicated-table-model.json"
-import normalizedComplicatedTableModel from "./assets/conceptual_complicated-table-model-normalized.json"
-import simpleTableModelList from "./assets/conceptual_simple-table-models.json";
+import complicatedTableModel from "./assets/conceptual_complicated-table-model.json";
+import complicatedTableModel_normalized from "./assets/conceptual_complicated-table-model-normalized.json";
+import complicatedTableModel_simplified from "./assets/conceptual_simple-table-models.json";
+
+import cityRankingTableModel from "./assets/fairy-city-ranking-table-model.json";
+import cityRankingTableModel_normalized from "./assets/fairy-city-ranking-table-normalized.json";
+import cityRankingTableModel_simplified from "./assets/fairy-city-ranking-table-simple-models.json";
+
 import { TableModelUtils} from "../src/index";
 
-test("shadow cells and header information for each cell table model are available", () => {
+test.concurrent.each([
+	[complicatedTableModel, complicatedTableModel_normalized],
+	[cityRankingTableModel, cityRankingTableModel_normalized]
+])("%s: shadow cells and header information for each cell table model are available", async (inputTableModel, expectedNormalizedOutputTableModel) => {
 	expect(function() {
 		// @ts-expect-error
-		TableModelUtils.addShadowCellsAndHeaderInformationToEachCell(complicatedTableModel);
-		return complicatedTableModel;
-	}()).toEqual(normalizedComplicatedTableModel);
+		TableModelUtils.addShadowCellsAndHeaderInformationToEachCell(inputTableModel);
+		return inputTableModel;
+	}()).toEqual(expectedNormalizedOutputTableModel);
 });
 
-test("simple table models out of a complicated table model can be generated", () => {
+test.concurrent.each([
+	[complicatedTableModel_normalized, complicatedTableModel_simplified],
+	[cityRankingTableModel_normalized, cityRankingTableModel_simplified]
+])("%s: simple table models out of a complicated table model can be generated", async (normalizedInputTableModel, expectedSimplifiedOutputTableModel) => {
 	expect(function() {
 		// @ts-expect-error
-		return TableModelUtils.generateSimpleTabelModelsOutOfComplicatedTableModel(normalizedComplicatedTableModel);
-	}()).toEqual(simpleTableModelList);
-})
+		return TableModelUtils.generateSimpleTabelModelsOutOfComplicatedTableModel(normalizedInputTableModel);
+	}()).toEqual(expectedSimplifiedOutputTableModel);
+});
